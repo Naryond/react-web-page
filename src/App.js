8650bfeb7131154460807ba8427614d6f4761087
Naryond <giuggey85@gmail.com>
2022-05-11 20:30:00 +0100

    Version 1.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body className="body">
+      <header className="header" id="title">
+        <h1 className="h1">Giuseppe Valeriani</h1>
+        <nav className="nav">
+          <div className="list">
+            <li className="li">
+              <a className="a" href="#title">
+                Home
+              </a>
+            </li>
+            <li className="li">
+              <a className="a" href="#background">
+                Background
+              </a>
+            </li>
+            <li className="li">
+              <a className="a" href="#portfolio">
+                Portfolio
+              </a>
+            </li>
+            <li className="li">
+              <a className="a" href="#contacts">
+                Contacts
+              </a>
+            </li>
+          </div>
+        </nav>
       </header>
-    </div>
+      <article id="home">
+        <div className="leveling">
+          <p className="p" id="info">
+            I am a Front End Developer based in London (U.K.)
+            <br />
+            Here some informations regarding myself and my career.{" "}
+          </p>
+          <div className="block1" id="img-div">
+            <img
+              className="pic1"
+              src="https://live.staticflickr.com/65535/52012232147_c8a96a5300_b.jpg"
+              alt="profile"
+            />
+          </div>
+        </div>
+      </article>
+      <article>
+        <div className="leveling">
+          <div className="block2">
+            <img
+              className="pic2"
+              src="https://tipsmake.com/data0/mimages/How-to-Make-an-Online-Casino-App-for-Free.jpg"
+              alt="slot"
+            />
+            <p className="p" id="background">
+              Born in Italy in 1985, I lived withing the country until 30 y/o
+              when I decided to change my live starting to study to become a
+              Casino Dealer. Having had 10 years of experience in the Gambling
+              Industry that was an easy step to make, and in 6 months I
+              succesfully completed my course and moved in the United Kingdom
+              where I immediately started to work./<br></br> After 6 years I
+              realize I needed a new professional challenge and decided to focus
+              more on what I love. Therefore I started to learn how to code.
+              <br></br>
+            </p>
+          </div>
+        </div>
+      </article>
+      <article id="portfolio">
+        <h2 className="h2">* * * Section in update * * *</h2>
+        <div className="block3">
+          <img
+            src="https://png.pngitem.com/pimgs/s/207-2073499_translate-platform-from-english-to-spanish-work-in.png"
+            alt="in update"
+          />
+        </div>
+      </article>
+      <div className="block4"></div>
+
+      <footer className="footer" id="contacts">
+        <a className="a" href="giuggey85@gmail.com">
+          E-mail
+        </a>
+        <a className="a" href="https://github.com/Naryond/Naryond">
+          Git Hub
+        </a>
+        <a
+          className="a"
+          href="https://www.linkedin.com/in/giuseppe-valeriani-4b9995115/"
+        >
+          LinkedIn
+        </a>
+      </footer>
+    </body>
   );
 }
 
