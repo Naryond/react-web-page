6ec7a5ab6ceed558f89f34599094d58d4e811e95
Naryond <giuggey85@gmail.com>
2022-07-23 15:57:25 +0100

    working on the UI

--- src/App.jsx
+++ src/App.jsx
@@ -39,34 +39,25 @@ function App() {
           </a>
         </div>
       </div>
-      <div>
+      <div className="personalInfo">
         <p>
           I am <strong>Giuseppe Valeriani</strong> and I am based in London
-          (U.K.)
+          (U.K.). Born in Italy in 1985, I lived withing the country until 30
+          y/o when I decided to change my live starting to study to become a
+          Casino Dealer. Having had 10 years of experience in the Gambling
+          Industry that was an easy step to make, and in 6 months I succesfully
+          completed my course and moved in the United Kingdom where I
+          immediately started to work.
+          <br></br> <br></br>
+          After 6 years I realize I needed a new professional challenge and
+          decided to focus more on what I love. Therefore I started to learn how
+          to code.
         </p>
-      </div>
-      <div>
-        <div>
-          <img
-            className="pic-size"
-            src="https://live.staticflickr.com/65535/52012232147_c8a96a5300_b.jpg"
-            alt="profile"
-          />
-        </div>
-        <div>
-          <p>
-            Born in Italy in 1985, I lived withing the country until 30 y/o when
-            I decided to change my live starting to study to become a Casino
-            Dealer. Having had 10 years of experience in the Gambling Industry
-            that was an easy step to make, and in 6 months I succesfully
-            completed my course and moved in the United Kingdom where I
-            immediately started to work.<br></br>
-            After 6 years I realize I needed a new professional challenge and
-            decided to focus more on what I love. Therefore I started to learn
-            how to code.
-            <br></br>
-          </p>
-        </div>
+        <img
+          className="img-size"
+          src="https://live.staticflickr.com/65535/52012232147_c8a96a5300_b.jpg"
+          alt="profile"
+        />
       </div>
       <footer>
         <a href="giuggey85@gmail.com" target="_blank">
